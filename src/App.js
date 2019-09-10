9e183f05416f5d8055c76f98b0f7d21bb32eb2f8
Yago Lopes <yagolopeslazaro@gmail.com>
2019-09-10 15:36:12 -0300

    header versão inicial

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const options = {
 function App() {
   return (
     <Provider template={AlertTemplate} {...options}>
-      {" "}
       <Routes />
     </Provider>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 import "./styles.css";
+import "../../assets/font/style.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div id="container">
+      <header>
+        <div id="title">
+          <h4>Blog e Podcast</h4>
+          <h1>FALA MARUJO</h1>
+          <h4>by Fragata Corp</h4>
+        </div>
+        <div id="info">
+          <p>
+            A melhor opção para uma tripulação que está a caça de informações
+            sobre tecnologia
+          </p>
+        </div>
+      </header>
+      <ul>
+        <li>Novidades</li>
+        <li>Sobre o Fala Marujo</li>
+        <li>Seija um Apoiador</li>
+        <li>Fragata Corp</li>
+      </ul>
+
+      <div id="search">
+        <input type="text" onChange={() => {}}></input>
+      </div>
+    </div>
+  );
 }
